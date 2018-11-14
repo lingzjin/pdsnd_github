2ea898150343f2c9aa160b520706e1109c5df9bc
MM <lingzjin@gmail.com>
2018-11-14 21:31:46 +0800

    Enhance description on Bikeshare file

--- Lingzi Jin Bikeshare1.py
+++ Lingzi Jin Bikeshare1.py
@@ -157,7 +157,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare user types and gender."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
